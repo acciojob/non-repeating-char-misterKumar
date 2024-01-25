7bb03b62a30290040e302208439b60dfacb881d6
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 19:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    if (str.length === 0) {
+        return null;
+    }
+
+    const charCountMap = new Map();
+
+    for (let char of str.toLowerCase()) {
+        charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
+    }
+
+    for (let char of str.toLowerCase()) {
+        if (charCountMap.get(char) === 1) {
+            return char;
+        }
+    }
+
+    return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+const result = firstNonRepeatedChar(input);
+
+result===null? alert("null"):alert(result)
+// if (result !== "null") {
+//     alert(result);
+// } else {
+//     alert(null);
+// }
